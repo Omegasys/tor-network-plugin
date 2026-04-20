60be9b921fca210ab149ffe62d49ba56da09daed
Omega <emomega@proton.me>
2026-04-19 23:36:26 -0400

    Create TorVpnManager.h

---
+++ linux/src/TorVpnManager.h
@@ -0,0 +1,8 @@
+// linux/src/TorVpnManager.h
+#ifndef TORVPNMANAGER_H
+#define TORVPNMANAGER_H
+
+void start_vpn();  // Starts the TOR service
+void stop_vpn();   // Stops the TOR service
+
+#endif // TORVPNMANAGER_H
